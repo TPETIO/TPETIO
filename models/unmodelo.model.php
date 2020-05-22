5c886030c636024ca80d8e63b738e5247f569bf6
Joselina Orellano <joselinaorellanog@gmail.com>
2020-05-22 19:38:31 -0300

    arregle el router

---
+++ models/unmodelo.model.php
@@ -0,0 +1,20 @@
+<?php
+
+class ClaseModelo{
+    private function crearConeccion(){
+        $host = 'localhost';
+        $userName = 'root';
+        $password = '';
+        $database = 'db_comandosGit';
+        $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$userName,$password);
+        return $pdo;
+    }
+
+    public function traerTodosComandos(){
+         $db = $this->crearConeccion();
+         $sentencia = $db->prepare("SELECT * FROM db_comandosGit");
+         $sentencia->execute();
+         $comandos = $sentencia->fetchAll(PDO::FETCH_OBJ);
+         return $comandos;
+    }
+}
