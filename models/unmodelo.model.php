5fbf4f28c590a35889bf4d7b37558dc55fd640df
Sasha Lujan <lujnannatasha1989@gmail.com>
2020-05-23 16:20:19 -0300

    modifique archivo

--- models/unmodelo.model.php
+++ models/unmodelo.model.php
@@ -5,14 +5,14 @@ class ClaseModelo{
         $host = 'localhost';
         $userName = 'root';
         $password = '';
-        $database = 'db_comandosGit';
+        $database = 'db_comandosgit';
         $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$userName,$password);
         return $pdo;
     }
 
     public function traerTodosComandos(){
          $db = $this->crearConeccion();
-         $sentencia = $db->prepare("SELECT * FROM db_comandosGit");
+         $sentencia = $db->prepare("SELECT * FROM db_comandosgit");
          $sentencia->execute();
          $comandos = $sentencia->fetchAll(PDO::FETCH_OBJ);
          return $comandos;
